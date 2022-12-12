f4f8f997c96d09fcb77ff489e9b763277c48e4de
eva2468 <abhishek.ev@mca.christuniversity.in>
2022-12-12 09:38:52 +0530

    commit f1

--- app.py
+++ app.py
@@ -2,18 +2,20 @@ from flask import Flask,request, url_for, redirect, render_template,flash
 import numpy as np
 import joblib
 import keras.utils as image
+from newsapi import NewsApiClient
 from keras.models import load_model
 import cv2
 
 
 app = Flask(__name__,static_url_path='/static')
+newsapi = NewsApiClient(api_key='70fdb9ba81ba40b6bda148e672898bd9')
 
 dic = {0 : 'dense', 1 : 'normal'}
 
 model_img =load_model('image_processing.h5')
 model=joblib.load('forestfiremodel.pkl')
 
-@app.route('/')
+@app.route('/dash')
 def dash():
     return render_template("dash.html")
 @app.route('/predict')
@@ -41,9 +43,26 @@ def predict_label(img_path):
     new_arr = cv2.resize(img, (60, 60))
     new_arr = np.array(new_arr)
     new_arr = new_arr.reshape(-1, 60, 60, 1)
-    return new_arr
-   
-    
+    return new_arr 
+# helper function
+def get_sources_and_domains():
+	all_sources = newsapi.get_sources()['sources']
+	sources = []
+	domains = []
+	for e in all_sources:
+		id = e['id']
+		domain = e['url'].replace("http://", "")
+		domain = domain.replace("https://", "")
+		domain = domain.replace("www.", "")
+		slash = domain.find('/')
+		if slash != -1:
+			domain = domain[:slash]
+		sources.append(id)
+		domains.append(domain)
+	sources = ", ".join(sources)
+	domains = ", ".join(domains)
+	return sources, domains
+
     
 @app.route('/image_dash',methods=['POST','GET'])
 def get_image():
@@ -53,7 +72,41 @@ def get_image():
         img.save(img_path)
         prediction =model_img.predict(predict_label(img_path))
     return render_template("image_dash.html",prediction = dic[prediction.argmax()], img_path = img_path)
- 
+@app.route("/", methods=['GET', 'POST'])
+def home():
+	if request.method == "POST":
+		sources, domains = get_sources_and_domains()
+		keyword = request.form["keyword"]
+		#keyword="sports"
+		related_news = newsapi.get_everything(q=keyword,
+									sources=sources,
+									domains=domains,
+									language='en',
+									sort_by='relevancy')
+		no_of_articles = related_news['totalResults']
+		if no_of_articles > 100:
+			no_of_articles = 100
+		all_articles = newsapi.get_everything(q=keyword,
+									sources=sources,
+									domains=domains,
+									language='en',
+									sort_by='relevancy',
+									page_size = no_of_articles)['articles']
+		return render_template("home.html", all_articles = all_articles,
+							keyword=keyword)
+	else:
+		top_headlines = newsapi.get_top_headlines(country="in", language="en")
+		total_results = top_headlines['totalResults']
+		if total_results > 100:
+			total_results = 100
+		all_headlines = newsapi.get_top_headlines(country="in",
+													language="en",
+													page_size=total_results)['articles']
+		return render_template("dash.html", all_headlines = all_headlines)
+	return render_template("dash.html")
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
